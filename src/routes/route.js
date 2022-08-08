0b5a66438aae409fed75b02d69065df93b1c9b26
miryala veronica <101985218+veronica17-web@users.noreply.github.com>
2022-08-08 13:09:13 +0530

    npm using

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require('lodash')
 const abc = require('../introduction/intro')
  const welcomming = require("../logger/logger")
   const info = require("../util/helper")
@@ -6,12 +7,28 @@ const abc = require('../introduction/intro')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    //console.log('My batch is', abc.name)
-   welcomming.wellcome()
-  info.mybatchinfo()
-  changing.fromatterchanges()
-  //  abc.printName()
+  //   console.log('My batch is', abc.name)
+  //  welcomming.wellcome()
+  // info.mybatchinfo()
+  // changing.fromatterchanges()
+  // abc.printName()
+let allmonths = ['jan','feb' ,'march','april','may','june','july','agust','sep','oct','nov','dec']
+    let result =lodash.chunk(allmonths,3)
+    console.log(result)
+
+   let odd = [1,3,5,7,9,11,13,15,17,19]
+   let allodd = lodash.tail(odd)
+   console.log(allodd)
+
+
+   // let allarr=[]
+    let ans = lodash.union([[1,2,3,3],[3,4,5],[5,6,7],[7,8,9],[10,11,11]])
+    console.log(ans)
+
+    let pairs = lodash.fromPairs([["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]])
+    console.log(pairs)
     res.send('My second ever api!')
+    
 });
 
 
