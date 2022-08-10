ec4f29595f88b4d31a0a9e3e1697920b873a6040
miryala veronica <101985218+veronica17-web@users.noreply.github.com>
2022-08-10 12:39:47 +0530

    prbm

--- src/routes/route.js
+++ src/routes/route.js
@@ -23,21 +23,7 @@ res.send({ data: missingNo  }  )
 
 
 
-// router.get("/sol1", function (req, res) {
-//     //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
-//     let arr= [1,2,3,5,6,7]
-  
-//     let total = 0;
-//     for (var i in arr) {
-//         total += arr[i];
-//     }
-  
-//     let lastDigit= arr.pop() // last number is the n so we pop it and saved it in variable
-//     let consecutiveSum= lastDigit * (lastDigit+1) / 2
-//     let missingNumber= consecutiveSum - total
-  
-//     res.send(  { data: missingNumber  }  );
-//   });
+
  
 router.get('/sol2',function(req,res){
     let arr =[33, 34, 35, 37, 38]
