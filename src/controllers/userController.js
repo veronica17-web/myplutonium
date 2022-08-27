fd6291b0dc8c67f4fef52e29ebac4b1712a068ef
miryala veronica <101985218+veronica17-web@users.noreply.github.com>
2022-08-27 23:33:58 +0530

    auth-token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,12 +5,10 @@ const userModel = require("../models/userModel");
   Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
 */
 const createUser = async function (abcd, xyz) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
+  
   let data = abcd.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
+ 
   xyz.send({ msg: savedData });
 };
 
@@ -44,26 +42,7 @@ const loginUser = async function (req, res) {
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
 
-  //If no token is present in the request header return error. This means the user is not logged in.
-  if (!token) return res.send({ status: false, msg: "token must be present" });
- 
-  console.log(token);  
-
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-
-  // Decoding requires the secret again. 
-  // A token can only be decoded successfully if the same secret was used to create(sign) that token.
-  // And because this token is only known to the server, it can be assumed that if a token is decoded at server then this token must have been issued by the same server in past.
-  let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
 
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
@@ -75,10 +54,7 @@ const getUserData = async function (req, res) {
 };
 
 const updateUser = async function (req, res) {
-  // Do the same steps here:
-  // Check if the token is present
-  // Check if the token present is a valid token
-  // Return a different error message in both these cases
+
 
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
@@ -88,11 +64,24 @@ const updateUser = async function (req, res) {
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData,{new:true});
+  res.send({ status: true, data: updatedUser });
 };
+
+
+const deleteUser = async function(req,res){
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId)
+  if (!user) {
+    return res.send("No such user exists");
+  }
+
+  let deletedUser = await userModel.findOneAndUpdate({ _id: userId },{isDeleted:true},{new:true});
+  res.send({ status: true, data: deletedUser });
+}
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser=deleteUser
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const mw = require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -11,8 +12,10 @@ router.post("/users", userController.createUser  )
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId",mw.tokenMW, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId",mw.tokenMW, userController.updateUser)
+
+router.delete("/users/:userId",mw.tokenMW,userController.deleteUser)
 
 module.exports = router;
